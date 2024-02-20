d2291c3cebf80e6b134fb4230bc861378b56c0de
Sabeena Viklar <viklarsabeena@gmail.com>
2024-02-20 23:42:16 +0530

    constants added

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_fasalMate_work', label: 'How fasalMate Work?' },
+  { href: '/', key: 'buy_Products', label: 'Buy Products' },
+  { href: '/', key: 'collaborate', label: 'Collaborate' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Accessible Farm Locations',
+    icon: '/map.png',
+    variant: 'green',
+    description:
+      'The real location of the farm is Accessible. You can easily navigate the Farm. ',
+  },
+  {
+    title: 'Calender schedule',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      "Schedule your timings and date",
+  },
+  // {
+  //   title: 'Technology using augment reality',
+  //   icon: '/tech.svg',
+  //   variant: 'green',
+  //   description:
+  //     ' ',
+  // },
+  {
+    title: 'Buy/Sell products',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Lots of Products to purchase/sale which is fresh and organic, in fair & Compensation price.',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About fasalmate',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Climbing xixixi', 'Hiking fasalmate', 'fasalmate kinthill'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '123-456-7890' },
+    { label: 'Email Officer', value: 'fasalmate@akinthil.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
